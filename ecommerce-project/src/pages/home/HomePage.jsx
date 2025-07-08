f8a000a4c7da13971c577a07bce480d17fc634a7
Emmanuel Ozoemena <ozoemenachukwuebuka2004@gmail.com>
2025-07-08 22:13:52 +0100

    Updated assets import

--- ecommerce-project/src/pages/home/HomePage.jsx
+++ ecommerce-project/src/pages/home/HomePage.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { Header } from "../../components/Header";
-import CheckmarkIcon from "../assets/images/icons/checkmark.png";
+import CheckmarkIcon from "../../assets/images/icons/checkmark.png";
 import { formatMoney } from "../../utils/money";
 import "./HomePage.css";
 
--- ecommerce-project/src/pages/orders/OrdersPage.jsx
+++ ecommerce-project/src/pages/orders/OrdersPage.jsx
@@ -1,5 +1,5 @@
 import { Header } from "../../components/Header";
-import BuyAgainIcon from '../assets/images/icons/buy-again.png';
+import BuyAgainIcon from "../../assets/images/icons/buy-again.png"
 import "./OrdersPage.css";
 
 export function OrdersPage() {
