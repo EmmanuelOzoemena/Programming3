866d66fbeb00587c48ae3bca96605c2daa5f3595
Emmanuel Ozoemena <ozoemenachukwuebuka2004@gmail.com>
2025-07-14 22:18:54 +0100

    Made axios available in the console

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -11,6 +11,8 @@ import "./App.css";
 function App() {
   const [cart, setCart] = useState([]);
 
+  window.axios = axios;
+
   const loadCart = async () => {
     const response = await axios.get("api/cart-items?expand=product");
     setCart(response.data);
@@ -23,9 +25,18 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<HomePage cart={cart} loadCart={loadCart} />} />
-        <Route path="checkout" element={<CheckoutPage cart={cart} loadCart={loadCart} />} />
-        <Route path="orders" element={<OrdersPage cart={cart} loadCart={loadCart} />} />
+        <Route
+          path="/"
+          element={<HomePage cart={cart} loadCart={loadCart} />}
+        />
+        <Route
+          path="checkout"
+          element={<CheckoutPage cart={cart} loadCart={loadCart} />}
+        />
+        <Route
+          path="orders"
+          element={<OrdersPage cart={cart} loadCart={loadCart} />}
+        />
         <Route
           path="tracking/:orderId/:productId"
           element={<TrackingPage cart={cart} />}
